ca3afb82698b5edd296988655bfece9b3a6bd42d
Lucas Fabris <gflucas23@gmail.com>
2024-09-30 19:07:21 -0300

    Continuacao do desenvolvimento

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ReactApexChart from 'react-apexcharts';
 import { IoThunderstorm } from "react-icons/io5";
 import { FiMenu } from "react-icons/fi";
 import '../home/home.css';
@@ -9,6 +10,19 @@ const Home = () => {
     const toggleMenu = () => {
         setIsActive(!isActive);
     };
+
+    const [chartOptions] = useState({
+        chart: {
+            type: 'line'
+        },
+        series: [{
+            name: 'sales',
+            data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
+        }],
+        xaxis: {
+            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+        }
+    });
 
     return (
         <div className="home-container">
@@ -30,7 +44,17 @@ const Home = () => {
                 </div>
             </nav>
 
-            <div className="dashboard">
+            <div className="App">
+                <ReactApexChart
+                    options={chartOptions}
+                    series={chartOptions.series}
+                    type="line"
+                    height={350}
+                />
+            </div>
+
+
+            {/* <div className="dashboard">
                 <div className="main-content">
                     <div className="header">
                         <h1>NPS Dashboard</h1>
@@ -62,7 +86,7 @@ const Home = () => {
                         <div>Net Promoter Score: 53</div>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 };
